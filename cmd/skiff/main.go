4239f300bed69387f9ca2b7b2766ed978a75caae
Dan Čermák <dcermak@suse.com>
2025-07-03 11:09:37 +0200

    properly switch to user namespace
    see: https://pkg.go.dev/github.com/containers/image/v5 section about running in
    rootless mode

--- cmd/skiff/main.go
+++ cmd/skiff/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/containers/storage/pkg/reexec"
 	"github.com/containers/storage/pkg/unshare"
+	"github.com/syndtr/gocapability/capability"
 
 	"github.com/urfave/cli/v3"
 )
@@ -17,8 +18,34 @@ func main() {
 		Name:  "skiff",
 		Usage: "Analyze the disk usage and directory structure of OCI images and its layers",
 		Before: func(ctx context.Context, c *cli.Command) (context.Context, error) {
+			var neededCapabilities = []capability.Cap{
+				capability.CAP_CHOWN,
+				capability.CAP_DAC_OVERRIDE,
+				capability.CAP_FOWNER,
+				capability.CAP_FSETID,
+				capability.CAP_MKNOD,
+				capability.CAP_SETFCAP,
+			}
+
 			reexec.Init()
-			unshare.MaybeReexecUsingUserNamespace(false)
+
+			// NewPid is deprecated so let's copy and paste its implementation 🙄
+			capabilities, err := capability.NewPid2(0)
+			if err != nil {
+				return ctx, err
+			}
+			err = capabilities.Load()
+			if err != nil {
+				return ctx, err
+			}
+
+			for _, cap := range neededCapabilities {
+				if !capabilities.Get(capability.EFFECTIVE, cap) {
+					// We miss a capability we need, create a user namespaces
+					unshare.MaybeReexecUsingUserNamespace(true)
+				}
+			}
+
 			return ctx, nil
 		},
 		Commands: []*cli.Command{&LayerUsage, &topCommand},
